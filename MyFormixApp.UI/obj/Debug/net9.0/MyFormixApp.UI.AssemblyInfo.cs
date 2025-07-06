cb60aa2cc37fc25e0439a000b5f677744082200e
SocioPhobe0 <annauhimuk@gmail.com>
2025-07-06 04:46:02 +0300

    language Themes part

--- MyFormixApp.UI/obj/Debug/net9.0/MyFormixApp.UI.AssemblyInfo.cs
+++ MyFormixApp.UI/obj/Debug/net9.0/MyFormixApp.UI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyFormixApp.UI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4f0c3b19d6269ee3f59fd639b77b44b18bef80b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87d80fe20866d32955a71442ac58fcb4016afe20")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyFormixApp.UI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyFormixApp.UI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
