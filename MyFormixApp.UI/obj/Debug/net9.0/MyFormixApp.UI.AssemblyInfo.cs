980465f25a0a71cf4db08e8c62a3c7e646acc692
SocioPhobe0 <annauhimuk@gmail.com>
2025-07-06 03:20:29 +0300

    language more files

--- MyFormixApp.UI/obj/Debug/net9.0/MyFormixApp.UI.AssemblyInfo.cs
+++ MyFormixApp.UI/obj/Debug/net9.0/MyFormixApp.UI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyFormixApp.UI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6fcea1ad571291cc33aeaf3316869c51732838a7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d31eb89fdb85e9b1e7e904b3da5df553872f733")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyFormixApp.UI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyFormixApp.UI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
